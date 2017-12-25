bc69bcc920b3c6a0e381a94784465b25a7ab6e52
Phan Truong Thanh <knightofk9@gmail.com>
2017-12-25 22:23:09 +0700

    Init

---
+++ Main.py
@@ -0,0 +1,10 @@
+import Constant
+import sys
+sys.path.append(Constant.DARK_NET_PYTHON_MODULE_PATH)
+import darknet
+from VideoExtractor import VideoExtractor
+
+# video_extractor = VideoExtractor()
+# video_extractor.extract_to(video_extractor.get_random_stream_url(), Constant.VIDEO_EXTRACT_OUTPUT_PATH, 0.5, 20, True)
+
+
---
+++ VideoExtractor.py
@@ -0,0 +1,53 @@
+import Constant
+import sys
+import cv2
+import os
+import shutil
+
+sys.path.append(Constant.EXTRACTOR_PYTHON_MODULE_PATH)
+from Extractor import Extractor
+
+
+class VideoExtractor:
+
+    def __init__(self):
+        self.extractor = Extractor()
+
+    def get_random_stream_url(self):
+        return self.extractor.get_random_avail_stream_file()
+
+    def reload_alive_stream_url(self):
+        self.extractor.check_all_avail_stream_file()
+
+    def extract_to(self, input_path, output_path, sampling_rate=0.5, max_capture_frame=0,
+                   clear_input_folder_if_exists=False):
+        if clear_input_folder_if_exists and os.path.isdir(output_path):
+            print "Clearing output folder {}".format(output_path)
+            shutil.rmtree(output_path)
+            os.mkdir(output_path)
+        skip_sec = 1 / sampling_rate
+        video = cv2.VideoCapture(input_path)
+        success, image = video.read()
+        count = 0
+        fps = video.get(cv2.CAP_PROP_FPS)
+        frame_rate = 1.0 / fps
+        current_time = 0
+        count_reading = 0
+        print "Start capturing at url {}".format(input_path)
+        print "FPS: {} ".format(fps)
+        print "Output: {}".format(output_path)
+        print "Sampling rate: {}".format(sampling_rate)
+        while success:
+            success, image = video.read()
+            count_reading += 1
+            current_time += frame_rate
+            if current_time >= skip_sec:
+                current_time = 0
+                number_name = "{0:05d}".format(count)
+                output_url = output_path + "frame_" + number_name + ".jpg";
+                cv2.imwrite(output_url, image)
+                print "Writing {}".format(output_url)
+                count += 1
+                if 0 < max_capture_frame <= count:
+                    break
+        print "Task completed, total {} files".format(count)
