79d61ba86a60b8decd5231aed4f5a692bc8a9ddc
Phan Truong Thanh <knightofk9@gmail.com>
2017-12-28 00:35:35 +0700

    Simplify project

--- Main.py
+++ Main.py
@@ -1,11 +1,5 @@
-import Constant
 from VideoExtractor import VideoExtractor
-from Detector import Detector
 
 video_extractor = VideoExtractor()
-video_extractor.extract_to(video_extractor.get_random_stream_url(), Constant.VIDEO_EXTRACT_OUTPUT_PATH, 0.5, 10, True)
+video_extractor.extract_to(video_extractor.get_random_stream_url(), "./out/", 0.5, 10, True)
 
-# detector = Detector(Constant.DARK_NET_CFG_PATH + "yolo.cfg",  "yolo.weights",
-#                     Constant.DARK_NET_CFG_PATH + "coco.data")
-# detector.detect_all_image_in_folder(Constant.IMAGE_DETECT_INPUT_PATH,
-#                                     Constant.IMAGE_DETECT_OUTPUT_PATH)
--- MainVideoExtractor.py
+++
@@ -1,5 +0,0 @@
-import Constant
-from VideoExtractor import VideoExtractor
-
-video_extractor = VideoExtractor()
-video_extractor.extract_to(video_extractor.get_random_stream_url(), "./out/", 0.5, 10)
